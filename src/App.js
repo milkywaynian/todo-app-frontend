6c11a78dfaa946bc5ec072e03f1fe499e044767a
milkywaynian <marina.faraji1@gmail.com>
2019-12-26 17:12:15 +0200

    playing with react

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Header from './Header';
+
+class myApp extends React.Component {
+
+  render() {
+      return (
+        <div>
+        <Header />
+        <p>Hello there!!!</p>
+        </div>
+      );
+  }
 }
 
-export default App;
+export default myApp; 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello world!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
